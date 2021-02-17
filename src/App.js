5abb9a50ccf67d3dc7dfe2f46899d814172f073c
Alex <alex.andrul10@gmail.com>
2021-02-17 22:13:53 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import { useRef, useState } from "react";
+import Hls from "hls.js";
+import data from "./data.json";
+import "./App.css";
 
 function App() {
+  const [videoUrl, setVideoUrl] = useState("");
+  const video = useRef(null);
+  const hls = new Hls();
+  hls.loadSource(videoUrl);
+  hls.attachMedia(video.current);
+  hls.on(Hls.Events.MANIFEST_PARSED, function () {
+    video.current.play();
+  });
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+        <h2>Please choose your stream</h2>
+        <select
+          onChange={(event) => {
+            setVideoUrl(event.target.value);
+          }}
+          style={{ marginBottom: 50, padding: 10 }}
+          defaultValue="default"
         >
-          Learn React
-        </a>
+          <option value="default" disabled>
+            Choose here
+          </option>
+          {data.streams.map((url) => (
+            <option key={url.id} value={url.source}>
+              {url.name}
+            </option>
+          ))}
+        </select>
+        <video ref={video} autoPlay={true} controls className="videoPlayer" />
       </header>
     </div>
   );
